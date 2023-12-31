dc23239d285327e594f1ea2cccc08ca5b9045ead
leowb <leowb@LEOSLAPTOP>
2023-12-31 17:34:30 +0000

    Make models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+	bio = models.TextField()
+
+	def __str__(self):
+		return self.user.username
+
+class Blog(models.Model):
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	time = models.DateTimeField()
+	title = models.CharField(max_length=200)
+	content = models.TextField()
+
+class Comment(models.Model):
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	time = models.DateTimeField()
+	content = models.TextField()
