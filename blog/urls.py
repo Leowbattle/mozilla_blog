c61780efee5553fa3d769983b71a735d9343ecfb
leowb <leowb@LEOSLAPTOP>
2024-01-01 15:34:37 +0000

    Add user creation

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
 	path("blogger/", views.BloggersView.as_view(), name="bloggers"),
 	path("blogger/<int:pk>", views.BloggerView.as_view(), name="blogger"),
 	path("<int:pk>", views.BlogView.as_view(), name="blog"),
-	path("<int:blogid>/submit_comment", views.submit_comment, name="submit_comment")
+	path("<int:blogid>/submit_comment", views.submit_comment, name="submit_comment"),
+	path("register", views.register_user, name="register"),
+	path("create_user", views.create_user, name="create_user"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 from django.http import HttpResponse
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.views import generic
 from .models import Profile, Blog, Comment
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
 
 class IndexView(generic.ListView):
 	template_name = "blog/index.html"
@@ -35,4 +36,26 @@ def submit_comment(request, blogid):
 	text = request.POST.get("comment-text")
 	blog = Blog.objects.get(pk=blogid)
 	Comment.objects.create(author=request.user, blog=blog, time=datetime.now(), content=text)
-	return redirect(request.GET["next"])
+	return redirect(request.GET.get("next", "/"))
+
+def register_user(request):
+	context = {"next": request.GET.get("next", "/")}
+	return render(request, "registration/register.html", context)
+
+def create_user(request):
+	username = request.POST["username"]
+	password = request.POST["password"]
+
+	if User.objects.filter(username=username).exists():
+		context = {
+			"next": request.GET.get("next", "/"), 
+			 "error": "User with that username already exists"
+		}
+		return render(request, "registration/register.html", context)
+
+	user = User.objects.create_user(username, None, password)
+	profile = Profile.objects.create(user=user, bio="")
+
+	login(request, user)
+
+	return redirect(request.GET.get("next", "/"))
